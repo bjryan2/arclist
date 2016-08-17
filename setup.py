af6b7e4c9a496c0dcddef3b5fd6fde63f5f33474
Brendan Ryan <bryan@pinterest.com>
2016-08-17 08:22:58 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'arclist',
   packages = ['arclist'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'lists open phabricator diffs and their build / accept statuses',
   author = 'Brendan Ryan',
   author_email = 'ryan.brendanjohn@gmail.com',
