9847bf49a528ea85803505ea1e495b21583b65bb
Brendan Ryan <bryan@pinterest.com>
2016-08-16 22:39:44 -0700

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'arclist',
+  packages = ['arclist'], # this must be the same as the name above
+  version = '0.1',
+  description = 'lists open phabricator diffs and their build / accept statuses',
+  author = 'Brendan Ryan',
+  author_email = 'ryan.brendanjohn@gmail.com',
+  url = 'http://brendanjryan.com',
+  download_url = 'http://brendanjryan.com',
+  keywords = [],
+  classifiers = [],
+)
